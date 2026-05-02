0cbaa1a9005c6978c3f887fccb6c2ac77b4de408
abdullahadeeel <abdullahadeel429@gmail.com>
2026-05-02 16:04:40 +0500

    remaping normal workflow for more speeeed

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,22 +1,53 @@
--- Git status (still useful as your main hub)
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Git status" })
-
--- Add ALL files (git add .)
-vim.keymap.set("n", "<leader>gaa", function()
-  vim.cmd("Git add .")
-end, { desc = "Git add all files" })
+-- Add all files
+vim.keymap.set("n", "<leader>ga", function()
+  vim.cmd("silent! !git add .")
+  print("Added all files")
+end)
 
 -- Commit
 vim.keymap.set("n", "<leader>gc", function()
-  vim.cmd("Git commit")
-end, { desc = "Git commit" })
-
--- Create new branch
-vim.keymap.set("n", "<leader>gbc", function()
-  vim.cmd("Git checkout -b ")
-end, { desc = "Git create branch" })
+  local msg = vim.fn.input("Commit message: ")
+  if msg ~= "" then
+    vim.cmd('silent! !git commit -m "' .. msg .. '"')
+    print("Committed: " .. msg)
+  end
+end)
 
 -- Pull
-vim.keymap.set("n", "<leader>gp", function()
-  vim.cmd("Git pull")
-end, { desc = "Git pull" })
+vim.keymap.set("n", "<leader>gpl", function()
+  vim.cmd("silent! !git pull")
+  print("Pulled changes")
+end)
+
+-- Push
+vim.keymap.set("n", "<leader>gps", function()
+  vim.cmd("silent! !git push")
+  print("Pushed changes")
+end)
+
+-- Create branch
+vim.keymap.set("n", "<leader>gb", function()
+  local name = vim.fn.input("Branch name: ")
+  if name ~= "" then
+    vim.cmd("silent! !git checkout -b " .. name)
+    print("Created branch: " .. name)
+  end
+end)
+
+-- Add remote
+vim.keymap.set("n", "<leader>gr", function()
+  local name = vim.fn.input("Remote name: ")
+  if name == "" then return end
+  local url = vim.fn.input("Remote URL: ")
+  if url == "" then return end
+
+  vim.cmd("silent! !git remote add " .. name .. " " .. url)
+  print("Added remote: " .. name)
+end)
+
+
+vim.keymap.set("n", "<leader>gpu", function()
+  local branch = vim.fn.system("git branch --show-current"):gsub("\n", "")
+  vim.cmd("Git push -u origin " .. branch)
+  print("Pushed with upstream: " .. branch)
+end)
