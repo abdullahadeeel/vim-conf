31c95b59542d8d9e4a708aca5bfa1c9b861bed3f
abdullahadeeel <abdullahadeel429@gmail.com>
2026-05-03 19:53:47 +0500

    something had to be done.

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,54 +1,11 @@
--- Add all files
+vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+
 vim.keymap.set("n", "<leader>ga", function()
--- Add all files
-vim.keymap.set("n", "<leader>gaa", function()
-  vim.cmd("Git add .")
-end)
+    vim.cmd('Git add .')
+    print("All files Added")
+end
+)
 
--- Commit with inline message
-vim.keymap.set("n", "<leader>gc", function()
-  local msg = vim.fn.input("Commit message: ")
-  if msg ~= "" then
-    vim.cmd('Git commit -m "' .. msg .. '"')
-    print("Committed: " .. msg)
-  end
-end)
-
--- Pull
-vim.keymap.set("n", "<leader>gl", function()
-  vim.cmd("Git pull")
-end)
-
--- Push
-vim.keymap.set("n", "<leader>gp", function()
-  vim.cmd("Git push")
-end)
-
--- Create new branch
-vim.keymap.set("n", "<leader>gb", function()
-  local name = vim.fn.input("Branch name: ")
-  if name ~= "" then
-    vim.cmd("Git checkout -b " .. name)
-    print("Switched to new branch: " .. name)
-  end
-end)
-
--- Add new remote
-vim.keymap.set("n", "<leader>gr", function()
-  local name = vim.fn.input("Remote name (e.g. origin): ")
-  if name == "" then return end
-
-  local url = vim.fn.input("Remote URL: ")
-  if url == "" then return end
-
-  vim.cmd("Git remote add " .. name .. " " .. url)
-  print("Added remote " .. name)
-end)
-  vim.cmd("silent! !git add .")
-  print("Added all files")
-end)
-
--- Commit
 vim.keymap.set("n", "<leader>gc", function()
   local msg = vim.fn.input("Commit message: ")
   if msg ~= "" then
@@ -58,13 +15,13 @@ vim.keymap.set("n", "<leader>gc", function()
 end)
 
 -- Pull
-vim.keymap.set("n", "<leader>gpl", function()
+vim.keymap.set("n", "<leader>gl", function()
   vim.cmd("silent! !git pull")
   print("Pulled changes")
 end)
 
 -- Push
-vim.keymap.set("n", "<leader>gps", function()
+vim.keymap.set("n", "<leader>gp", function()
   vim.cmd("silent! !git push")
   print("Pushed changes")
 end)
